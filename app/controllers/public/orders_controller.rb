f472d8c322d85d9988c49ecc6ddd9ec8bb13da3d
Takumi345 <t.takahashi345@gmail.com>
2022-05-23 15:23:26 +0000

    3rd

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,24 +6,22 @@ class Public::OrdersController < ApplicationController
 
   def confirm
     @order = Order.new(order_params)
-    @address = Address.find(params[:order][:address_id])
-    @order.postal_code = @address.postal_code
-    @order.address = @address.address
-    @order.name = @address.name
+    @cart_items = current_customer.cart_items
 
-    if @address_number = Address.find(params[:order][:address_number]) == "0"
+    #if @address_number = Address.find(params[:order][:address_number]) == "0"
       @order.postal_code = current_customer.postal_code
       @order.address = current_customer.address
-      @order.name = current_customer.name
-    if @address_number = Address.find(params[:order][:address_number]) == "1"
-      @order.postal_code = @address.postal_code
-      @order.address = @address.address
-      @order.name = @address.name
-    if @address_number = Address.find(params[:order][:address_number]) == "2"
-      @order.postal_code = @order.new
-      @order.address = @order.new
-      @order.name = @order.new
+      @order.name = current_customer.full_name
 
+    #elsif @address_number = Address.find(params[:order][:address_number]) == "1"
+      #@order.postal_code = @address.postal_code
+      #@order.address = @address.address
+      #@order.name = @address.name
+
+    #elsif @address_number = Address.find(params[:order][:address_number]) == "2"
+      #@order.postal_code = @order.new
+      #@order.address = @order.new
+      #@order.name = @order.new
   end
 
   def complete
@@ -32,7 +30,7 @@ class Public::OrdersController < ApplicationController
   def create
     @order = Order.new(order_params)
     @order.save
-    redirect_to public_orders_confirm(@order.id)
+    redirect_to public_orders_complete_path
   end
 
   def index
