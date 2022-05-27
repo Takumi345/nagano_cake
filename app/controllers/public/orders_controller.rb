cccd888edcbf067fbea81c08a83e36a9b9b24808
Takumi345 <t.takahashi345@gmail.com>
2022-05-27 22:55:52 +0000

    3rd

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -7,21 +7,19 @@ class Public::OrdersController < ApplicationController
   def confirm
     @order = Order.new(order_params)
     @cart_items = current_customer.cart_items
-
-    #if @address_number = Address.find(params[:order][:address_number]) == "0"
+    @address = Address.find(params[:order][:address_id])
+    @address_number = params[:order][:address_number]
+    if @address_number == "0"
       @order.postal_code = current_customer.postal_code
       @order.address = current_customer.address
       @order.name = current_customer.full_name
 
-    #elsif @address_number = Address.find(params[:order][:address_number]) == "1"
-      #@order.postal_code = @address.postal_code
-      #@order.address = @address.address
-      #@order.name = @address.name
+    elsif @address_number == "1"
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
 
-    #elsif @address_number = Address.find(params[:order][:address_number]) == "2"
-      #@order.postal_code = @order.new
-      #@order.address = @order.new
-      #@order.name = @order.new
+    end
   end
 
   def complete
@@ -30,19 +28,32 @@ class Public::OrdersController < ApplicationController
   def create
     @order = Order.new(order_params)
     @order.save
+    @cart_items = current_customer.cart_items.all
+    @cart_items.each do |cart_item|
+    @order_detail = OrderDetail.new
+    @order_detail.order_id = @order.id
+    @order_detail.item_id = cart_item.item_id
+    @order_detail.price = cart_item.item.price
+    @order_detail.amount = cart_item.amount
+    @order_detail.making_status = 0
+    @order_detail.save
+    end
     redirect_to public_orders_complete_path
   end
 
   def index
+    @orders = current_customer.orders.all
   end
 
   def show
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details.all
   end
 
 
   private
 
   def order_params
-    params.require(:order).permit(:payment_method,:postal_code, :address, :name, :customer_id)
+    params.require(:order).permit(:payment_method,:postal_code, :address, :name, :customer_id, :shipping_cost, :total_payment, :status)
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,7 @@ class Order < ApplicationRecord
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum status: { awaiting_payment: 0, payment_confirmation: 1, production: 2, shipping_preparation: 3, shipped: 4 }
 
-  has_many :order_detail, dependent: :destroy
+  has_many :order_details, dependent: :destroy
   belongs_to :customer
 
 end
