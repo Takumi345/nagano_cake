8f2b682b3223782ce4c75e15c6977c26f3ba6765
Takumi345 <t.takahashi345@gmail.com>
2022-05-29 11:35:11 +0000

    finish1

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -18,7 +18,7 @@ class Public::CustomersController < ApplicationController
 
   def withdraw
     @customer = current_customer
-    @customer.update(is_deleted: true)
+    @customer.update(is_deleted: false)
     sign_out current_customer
     redirect_to root_path
   end
